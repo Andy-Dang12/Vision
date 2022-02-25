9301c246c8d0577b0dfda14bda29e4e0da263d92
anhdv12 <imnova1212@gmail.com>
2022-02-25 10:14:28 +0700

    generate color

---
+++ Lib/color.py
@@ -0,0 +1,17 @@
+import colorsys
+import numpy as np
+
+
+def generate_color(num_Color:int) -> list:
+    """ 
+    https://github.com/qqwweee/keras-yolo3/blob/master/yolo.py
+    L82 - L91
+    """
+    hsv_tuples = [(x / num_Color, 1., 1.) for x in range(num_Color)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    np.random.seed(10101)       # Fixed seed for consistent colors across runs.
+    np.random.shuffle(colors)   # Shuffle colors to decorrelate adjacent classes.
+    np.random.seed(None)        # Reset seed to default
+
+    return colors
---
+++ Lib/encryption.py
@@ -0,0 +1,28 @@
+from base64 import b64decode, b64encode
+from typing import overload
+from PIL import Image
+from io import BytesIO
+from os.path import isfile
+
+class Ecryption_Image(object):
+
+    @staticmethod
+    def encode(image:Image) -> str:
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        return b64encode(buffered.getvalue())
+
+    @overload
+    @staticmethod
+    def encode(image:str) -> str:
+        if not isfile(image):
+            raise Exception("cannot found image at " + image)
+        
+        with open(image, "rb") as f:
+            converted_string = b64encode(f.read())
+        return converted_string
+
+    @staticmethod
+    def decode(encoded_str:str) -> Image:
+        return Image.open(BytesIO(b64decode(encoded_str)))
+
