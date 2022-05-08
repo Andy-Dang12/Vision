0e5eda3951fd894d3b0e17f1c2ec1cc7a1a05bc3
watson21 <imnova1212@gmail.com>
2022-05-08 10:49:20 +0700

    cv2.Stitcher_create

--- image_stitching.py
+++ image_stitching.py
@@ -34,15 +34,18 @@ list_kp2 = [kp2[mat.trainIdx].pt for mat in matches]
 
 #NOTE 3. Perspectivate transform - estimate homography matrix
 # Estimate homography matrix with RANSAC.
-H, status = cv2.findHomography(
-                np.float32(list_kp1), 
-                np.float32(list_kp2), 
-                cv2.RANSAC )
+H, status = cv2.findHomography(np.float32(list_kp1), 
+                               np.float32(list_kp2), cv2.RANSAC)
 
 h1, w1, c1 = img1.shape
 h2, w2, c2 = img2.shape
 result = cv2.warpPerspective(img1, H, (w1+w2, h1))
 result[0:h2, 0:w2] = img2
 
-# cv2.imshow('fa', result)
-# cv2.waitKey(0)
+
+# using opencv Stitcher_create
+stitcher = cv2.Stitcher_create()
+status, stitched = stitcher.stitch([img1, img2])
+if status == 0:
+	cv2.imshow('dw',stitched)
+	cv2.waitKey(0)
