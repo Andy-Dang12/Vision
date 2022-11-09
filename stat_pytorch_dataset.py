a2d47b3c4c2d06a04f2f869e31186082556b50be
agent12 <imnova1212@gmail.com>
2022-11-09 16:34:38 +0700

    calculate mean n std using pytorch
    https://towardsdatascience.com/how-to-calculate-the-mean-and-standard-deviation-normalizing-datasets-in-pytorch-704bd7d05f4c

---
+++ stat_pytorch_dataset.py
@@ -0,0 +1,34 @@
+import torch, os
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+
+
+path = '/home/tz/Downloads/data/14_11'
+dataset = ImageFolder(path, transform=ToTensor())
+dataLoader = DataLoader(dataset, 64, num_workers=os.cpu_count())
+
+
+def get_mean_and_std(dataloader:DataLoader):
+    channels_sum, channels_squared_sum, num_batches = 0, 0, 0
+    for data, _ in dataloader:
+        # Mean over batch, height and width, but not over the channels
+        channels_sum += torch.mean(data, dim=[0, 2, 3])
+        channels_squared_sum += torch.mean(data**2, dim=[0, 2, 3])
+        num_batches += 1
+    
+    mean = channels_sum / num_batches
+
+    # std = sqrt(E[X^2] - (E[X])^2)
+    std = (channels_squared_sum / num_batches - mean ** 2) ** 0.5
+
+    return mean, std
+
+from time import time
+
+s = time()
+mean, std = get_mean_and_std(dataLoader)
+ss = time()
+print(mean)
+print(std)
+print(ss-s)
