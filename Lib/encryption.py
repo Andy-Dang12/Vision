7b74b04eaa880c42e8ba0d23b6eab5cc7a8a9bfc
agent12 <imnova1212@gmail.com>
2023-02-22 11:47:16 +0700

    Update encryption.py
    fix encode B64 return str, optim speed, compress without lossy compression

--- Lib/encryption.py
+++ Lib/encryption.py
@@ -1,5 +1,7 @@
 from base64 import b64decode, b64encode
-from typing import overload
+from typing import overload, Union
+from binascii import b2a_base64
+from colorama import Fore
 from PIL import Image, ImageFile
 from io import BytesIO
 from os.path import isfile
@@ -35,24 +37,24 @@ def encode_pil(image:Image) -> str:
     image.save(buffered, format="JPEG")
     return b64encode(buffered.getvalue())
 
-
 def decode_pil(encoded_str:str) -> ImageFile.ImageFile:
     return Image.open(BytesIO(b64decode(encoded_str)))
 
 
+#https://tinyurl.com/ye2xxt5k
 def encode_cv2(image:cv2.Mat) -> str:
-    retval, buffer = cv2.imencode('.jpg', image)
-    return b64encode(buffer)
+    retval, buffer = cv2.imencode('.png', image, [cv2.IMWRITE_PNG_COMPRESSION, 9])
+    return b2a_base64(buffer.tobytes(), newline=False).decode('ascii', 'strict')
 
-
-def decode_cv2(encoded_str:str) -> cv2.Mat:
+def decode_cv2(encoded_str:Union[str, bytes]) -> cv2.Mat:
     im_bytes = b64decode(encoded_str)
     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)    # im_arr is one-dim Numpy array
     return cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
 
 
 if __name__ == '__main__':
-    imgp = '/home/tz/Documents/Pytorch_Retinaface/data/widerface/val/images/6--Funeral/6_Funeral_Funeral_6_627.jpg'
+    imgp = 'data/14_11_face_n_feat/face/Duc/Duc0000.jpg'
+    img = cv2.imread(imgp)
     
     def test_base64_cv2(imgp:str=imgp):
         cv = cv2.imread(imgp)
@@ -69,6 +71,16 @@ if __name__ == '__main__':
         pil_str = encode_pil(pil)
         img = decode_pil(pil_str)
         img.show(title='pil_decode')
+        
+    def test_compress_without_lossy_compression():
+        encoded_img = encode_cv2(img)
+        decoded_img = decode_cv2(encoded_img)
+
+        if np.array_equal(img, decoded_img):
+            print(Fore.GREEN + "The original and decoded images are the same." + Fore.RESET)
+        else:
+            print(Fore.RED + "The original and decoded images are different." + Fore.RESET)
     
     test_base64_cv2()
     test_base64_pil()
+    test_compress_without_lossy_compression()
